043344aea9aea97fb580c26489ce5829ec99a189
Prerith Subramanya <prerithsubramanya@gmail.com>
2023-02-15 12:25:49 +1100

    added api_url and run.sh

--- pcconfig.py
+++ pcconfig.py
@@ -2,7 +2,7 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="app",
-    # api_url="http://qr-app-backend.qlub.cloud",
-    api_url="http://localhost:8000",
+    api_url="http://qr-app-backend.qlub.cloud",
+    # api_url="http://localhost:8000",
     env=pc.Env.PROD,
 )
